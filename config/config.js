555191f7f3624ce2894376ad84ffe51d97a68738
shubhambarthwal <131369095+shubhambarthwal@users.noreply.github.com>
2023-07-04 15:33:32 +0530

    awesr

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ mongoose
   .connect(
     process.env.DATABASE  /* here singeimage is name of data base */
   )
+  //done
   .then((e) => console.log("config Connected"))
   .catch((e) => console.log("ERRORRRRRRRRRRRRRRRRRRRRRR"));
