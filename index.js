10dcc7851ed94f7d3c2126299436cef2e3276271
shubhambarthwal <131369095+shubhambarthwal@users.noreply.github.com>
2023-07-04 15:13:09 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv"); 
+dotenv.config();
 
 app.use(express.json());
 require("./config/config")
+
+
+
+mongoose
+  .connect(process.env.DATABASE)  
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => console.log("not connect"));
 
 
 const sign_in_route = require("./Route/signup")
@@ -14,5 +26,10 @@ app.use('/log',log_in_route)
 const phonedetails = require("./Route/phone"); 
 app.use("/hello", phonedetails);
 
-app.listen(3000);
-console.log("index connected");       
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+}); 
+            
+console.log("index file connected");       
